8c13f611a5521cc1f42dd26ca52c1aa576a4df3e
Tom Russell <TR115251@hotmail.co.uk>
2019-05-14 14:58:33 +0100

    init

---
+++ messenja-client-react/src/App.js
@@ -0,0 +1,8 @@
+// client/src/App.js
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>I'M READY TO USE THE BACK END APIS! :-)</div>;
+  }
+}
