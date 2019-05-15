e3e266b6b97e27d52f5ebde7ce3c9bcf42e7ab25
Tom Russell <TR115251@hotmail.co.uk>
2019-05-15 22:11:47 +0100

    refactor: test clean up

--- messenja-backend-express/test/app.integration.test.js
+++ messenja-backend-express/test/app.integration.test.js
@@ -1,30 +1,27 @@
 const request = require('supertest');
 const app = require('../app');
 
-// API should be able to recieve messages
-
-
-describe('POST /messages', function () {
-  let data = {
-    "content": "test-message"
-  }
-  it('respond with 201 created', function (done) {
-    request(app)
-    .post('/messages')
-    .send(data)
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(201)
-    .end((err) => {
-      if (err) return done(err);
-      done();
+// API should be able to post messages
+context('POST /messages', function functionName() {
+  describe('success', function () {
+    let data = {
+      "content": "test-message"
+    }
+    it('respond with 201 created', function (done) {
+      request(app)
+      .post('/messages')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
     });
   });
-});
-
-// API errors on bad message post
-
-  describe('POST /messages', function () {
+  // API errors on bad message post
+  describe('error', function () {
     let data = {
       "content": ""
     }
@@ -42,46 +39,44 @@ describe('POST /messages', function () {
       });
     });
   });
-
+});
 // API should be able to display all messages
-
-describe('GET /messages', function () {
-  it('respond with json containing a list of all messages', function (done) {
-    request(app)
-    .get('/messages')
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200, done);
-  });
-});
-
 // API can return a single message from given message ID
-
-describe('GET /messages/:id', function () {
-  it('respond with json containing a single message', function (done) {
-    request(app)
-    .get('/messages/message/:id')
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200, done);
-  });
-});
-
-describe('GET /messages/:id', function () {
-  let data = {
-    "id": "false_id"
-  }
-  it('respond with json message not found', function (done) {
-    request(app)
-    .get('/messages/message/idisnonexisting')
-    .send(data)
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(404) //expecting HTTP status code
-    .expect({error: "message not found"}) // expecting content value
-    .end((err) => {
-      if (err) return done(err);
-      done();
+context('GET /messages', function () {
+  describe('all', function () {
+    it('respond with json containing a list of all messages', function (done) {
+      request(app)
+      .get('/messages')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
     });
   });
-});
+  describe('success', function () {
+    it('respond with json containing a single message', function (done) {
+      request(app)
+      .get('/messages/message/:id')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+    });
+  });
+  describe('failure', function () {
+    let data = {
+      "id": "false_id"
+    }
+    it('respond with json message not found', function (done) {
+      request(app)
+      .get('/messages/message/idisnonexisting')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404) //expecting HTTP status code
+      .expect({error: "message not found"}) // expecting content value
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+    });
+  });
+})
