1deed03b417ed5a522b69ea76a1fbfe26c2d06a8
Tom Russell <TR115251@hotmail.co.uk>
2019-05-17 12:19:59 +0100

    Cypress: framework added

---
+++ messenja-client-react/cypress/integration/messenja-e2e.spec.js
@@ -0,0 +1,17 @@
+const serverUrl = Cypress.env('serverUrl');
+
+describe('messenja app - e2e', () => {
+
+  beforeEach(() => {
+  });
+
+  it('should display the message list', () => {
+  });
+
+  it('should add a new todo to the list', () => {
+  });
+
+  it('should update a message correctly', () => {
+  });
+
+});
